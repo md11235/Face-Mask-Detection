6f5f9ab277eaf4fbbe0bfd83c223fd9161cb800b
Sam <md11235@gmail.com>
2021-12-05 17:05:14 +0800

    set LR=0.001
                      pred: AroundNeck  pred: BelowNose  pred: Correct  pred: NoMask
    true: AroundNeck                20                7             36            22
    true: BelowNose                  7                4             28             4
    true: Correct                   18                3             85             9
    true: NoMask                     7                2              8            41

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -17,13 +17,20 @@ from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.utils import to_categorical
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, confusion_matrix
 from imutils import paths
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import argparse
 import os
 from sklearn.preprocessing import OneHotEncoder
+
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+import tensorflow as tf
+
+tf.keras.backend.set_image_data_format('channels_last')
+
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -38,9 +45,9 @@ args = vars(ap.parse_args())
 
 # initialize the initial learning rate, number of epochs to train for,
 # and batch size
-INIT_LR = 1e-4
+INIT_LR = 1e-3
 EPOCHS = 200000
-BS = 16
+BS = 32
 
 # grab the list of images in our dataset directory, then initialize
 # the list of data (i.e., images) and class images
@@ -77,8 +84,14 @@ encoder = OneHotEncoder(sparse=False)
 labels = labels.reshape((-1, 1))
 labels = encoder.fit_transform(labels)
 
-print(labels)
-print(labels.shape)
+print(encoder.inverse_transform(np.asarray([[0., 0., 0., 1.]])))
+print(encoder.inverse_transform(np.asarray([[0., 0., 1., 0.]])))
+print(encoder.inverse_transform(np.asarray([[0., 1., 0., 0.]])))
+print(encoder.inverse_transform(np.asarray([[1., 0., 0., 0.]])))
+
+# exit()
+# print(labels)
+# print(labels.shape)
 
 #print(encoder.categories_)
 
@@ -86,8 +99,14 @@ print(labels.shape)
 
 # partition the data into training and testing splits using 75% of
 # the data for training and the remaining 25% for testing
-(trainX, testX, trainY, testY) = train_test_split(data, labels,
-	test_size=0.20, stratify=labels, random_state=42)
+(trainX, testX, trainY, testY) = train_test_split(
+    data, labels,
+	test_size=0.20, stratify=labels, random_state=1337)
+
+# print(np.unique(trainY, axis=0, return_counts=True))
+# print(np.unique(testY, axis=0, return_counts=True))
+
+# exit()
 
 # construct the training image generator for data augmentation
 aug = ImageDataGenerator(
@@ -125,44 +144,71 @@ for layer in baseModel.layers:
 # compile our model
 print("[INFO] compiling model...")
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
-model.compile(loss="binary_crossentropy", optimizer=opt,
+model.compile(loss="categorical_crossentropy", optimizer=opt,
 	metrics=["accuracy"])
 
 # train the head of the network
 print("[INFO] training head...")
+early_stopping = EarlyStopping(monitor='val_loss', min_delta=0.000001, patience=100)
+model_checkpoint =  ModelCheckpoint("trained_models/" + 'mobilenet_v2_face_epoch_{epoch:09d}_loss{val_loss:.4f}.h5',
+                                    monitor='val_loss',
+                                    verbose=1,
+                                    save_best_only=True,
+                                    save_weights_only=False,
+                                    mode='auto',
+                                    period=1)
 H = model.fit(
 	aug.flow(trainX, trainY, batch_size=BS),
 	steps_per_epoch=len(trainX) // BS,
 	validation_data=(testX, testY),
 	validation_steps=len(testX) // BS,
-	epochs=EPOCHS)
+	epochs=EPOCHS,
+	callbacks=[early_stopping, model_checkpoint]
+)
 
 # make predictions on the testing set
 print("[INFO] evaluating network...")
 predIdxs = model.predict(testX, batch_size=BS)
 
+print(predIdxs)
 # for each image in the testing set we need to find the index of the
 # label with corresponding largest predicted probability
 predIdxs = np.argmax(predIdxs, axis=1)
+print(predIdxs)
 
-# show a nicely formatted classification report
-print(classification_report(testY.argmax(axis=1), predIdxs,
-	                        target_names=encoder.categories_[0]))
+print(testY.argmax(axis=1))
+# show a nicely formatted confusion matrix
+cat_names = encoder.categories_[0]
+
+cmtx = pd.DataFrame(
+    confusion_matrix([cat_names[ind] for ind in testY.argmax(axis=1)],
+                     [cat_names[ind] for ind in predIdxs],
+                     labels=cat_names),
+    index=["true: {}".format(cn) for cn in cat_names],
+    columns=["pred: {}".format(cn) for cn in cat_names]
+)
+
+print(cmtx)
+
+# y_true = pd.Series([2, 0, 2, 2, 0, 1, 1, 2, 2, 0, 1, 2])
+# y_pred = pd.Series([0, 0, 2, 1, 0, 2, 1, 0, 2, 0, 2, 2])
+
+# pd.crosstab(y_true, y_pred, rownames=['True'], colnames=['Predicted'], margins=True)
 
 # serialize the model to disk
 print("[INFO] saving mask detector model...")
 model.save(args["model"], save_format="h5")
 
 # plot the training loss and accuracy
-N = EPOCHS
-plt.style.use("ggplot")
-plt.figure()
-plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
-plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
-plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
-plt.title("Training Loss and Accuracy")
-plt.xlabel("Epoch #")
-plt.ylabel("Loss/Accuracy")
-plt.legend(loc="lower left")
-plt.savefig(args["plot"])
+# N = EPOCHS
+# plt.style.use("ggplot")
+# plt.figure()
+# plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+# plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+# plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
+# plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+# plt.title("Training Loss and Accuracy")
+# plt.xlabel("Epoch #")
+# plt.ylabel("Loss/Accuracy")
+# plt.legend(loc="lower left")
+# plt.savefig(args["plot"])
